5a91319d81ee3b3219305e81b6dcec24823e0f5b
Joe Edelman <joe@citizenlogistics.com>
2010-09-08 16:17:35 -0400

    run functions in the subclass context

--- lib/configurer.rb
+++ lib/configurer.rb
@@ -9,11 +9,11 @@ module Configurer
 
   def config *syms, &blk
     return @configurer if syms.empty?
-    configurer = @configurer ||= HashMM.new{|h,k| WORLDWIDE[k] || blk }
+    configurer = @configurer ||= HashMM.new{|h,k| ::WORLDWIDE[k] || blk }
     syms.each do |sym|
       meth = proc{ configurer[sym].call }
-      define_method(sym, &meth)
-      (class << self;self;end).send(:define_method, sym, &meth)
+      define_method(sym){ self.class.instance_exec &configurer[sym] }
+      (class << self;self;end).send(:define_method, sym){ instance_exec &configurer[sym] }
     end
   end
 end
--- test/test_configurer.rb
+++ test/test_configurer.rb
@@ -24,4 +24,29 @@ class TestConfigable < Test::Unit::TestCase
     Foo.new.try
     raise unless THING.first == :bye
   end
+
+  should "work without a global config" do
+    class Foo2
+      extend Configurer; config :baz do :bad end
+    end
+
+    x = Foo2.new
+    assert_equal :bad, Foo2.baz
+    assert_equal :bad, x.baz
+
+    Foo2.config.baz { :good }
+    assert_equal :good, Foo2.baz
+    assert_equal :good, x.baz
+  end
+
+  should "run in the subclass context" do
+    class ::A; extend Configurer; config :f do name end; end
+    class ::B < ::A; end
+
+    assert_equal 'A', A.f
+    assert_equal 'A', A.new.f
+    assert_equal 'B', B.f
+    assert_equal 'B', B.new.f
+  end
+
 end
